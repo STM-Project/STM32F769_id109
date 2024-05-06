4c6e10a138ec9eccca1531860565eaefe494f389
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-05-06 13:27:40 +0200

    Update SCREEN_CalibrationLCD.h
    blobal macro probe

--- Application/LCD/Screens/SCREEN_CalibrationLCD.h
+++ Application/LCD/Screens/SCREEN_CalibrationLCD.h
@@ -8,6 +8,10 @@
 #ifndef LCD_SCREENS_SCREEN_CALIBRATIONLCD_H_
 #define LCD_SCREENS_SCREEN_CALIBRATIONLCD_H_
 
+#define aaa(str) SCREEN_Calibration_##str
+
+extern struct NAZWA aaa(struct);  //w varioable.c odnies sie do tego tu miejsca poprzez *ptr
+
 void Touchscreen_Calibration(void);
 
 #endif /* LCD_SCREENS_SCREEN_CALIBRATIONLCD_H_ */
