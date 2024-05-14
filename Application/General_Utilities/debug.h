cf087bfb3d71bc9f128de5cd9ef40a586662674c
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-05-14 16:18:56 +0200

    Update debug.h

--- Application/General_Utilities/debug.h
+++ Application/General_Utilities/debug.h
@@ -8,10 +8,42 @@
 #ifndef GENERAL_UTILITIES_DEBUG_H_
 #define GENERAL_UTILITIES_DEBUG_H_
 
+#include "stm32f7xx_hal.h"
+
+typedef enum{
+	font,
+	bkg
+}FONT_BKG_COLOR;
+
+#define Clr_	"\x1B\x5B\x32\x4A"
+
+#define Red_	"\x1b[31m"
+#define Gre_	"\x1b[32m"
+#define Yel_	"\x1b[33m"
+#define Blu_	"\x1b[34m"
+#define Mag_	"\x1b[35m"
+#define Cya_	"\x1b[36m"
+
+#define Co1_	"\x1b[38;2;255;255;255m"
+#define Co2_	"\x1b[38;2;255;255;255m"
+#define Co3_	"\x1b[38;2;255;255;255m"
+
+#define Bk1_	"\x1b[48;2;255;255;255m"
+#define Bk2_	"\x1b[48;2;255;255;255m"
+#define Bk3_	"\x1b[48;2;255;255;255m"
+
+#define Bol_	"\x1b[1m"
+#define Ita_	"\x1b[4m"
+#define _X		"\x1b[0m"
+
+#define Col_	"%s"
+char* _Col(FONT_BKG_COLOR background, uint8_t red, uint8_t green, uint8_t blue);
+
 void DEBUG_Init(void);
 void Dbg(int on, char *txt);
 void DbgMulti(int on, char *startTxt, char *txt, char *endTxt);
 void DbgVar(int on, unsigned int buffLen, const char *fmt, ...);
+void DbgVar2(int on, unsigned int buffLen, const char *fmt, ...);
 void DEBUG_RxFullBuffService(void);
 int DEBUG_RcvStr(char *txt);
 
