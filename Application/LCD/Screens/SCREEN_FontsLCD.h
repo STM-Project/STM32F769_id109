52721f996f6410b4e96861974611863bca46bad6
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-05-16 15:05:23 +0200

    Add files via upload

---
+++ Application/LCD/Screens/SCREEN_FontsLCD.h
@@ -0,0 +1,15 @@
+/*
+ * SCREEN_FontsLCD.h
+ *
+ *  Created on: May 16, 2024
+ *      Author: RafałMarkielowski
+ */
+
+#ifndef LCD_SCREENS_SCREEN_FONTSLCD_H_
+#define LCD_SCREENS_SCREEN_FONTSLCD_H_
+
+#include "stm32f7xx_hal.h"
+
+void SCREEN_Fonts_main(void);
+
+#endif /* LCD_SCREENS_SCREEN_FONTSLCD_H_ */
