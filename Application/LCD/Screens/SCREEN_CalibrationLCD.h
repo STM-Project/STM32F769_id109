d3f8b0af315ccf90cd1f82028ca96fb2f36f199b
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-05-23 15:57:26 +0200

    Update SCREEN_CalibrationLCD.h

--- Application/LCD/Screens/SCREEN_CalibrationLCD.h
+++ Application/LCD/Screens/SCREEN_CalibrationLCD.h
@@ -12,31 +12,31 @@
 #include "stm32f7xx_hal.h"
 
 typedef enum{
-	_FONT_SIZE_Title,
-	_FONT_SIZE_CircleName,
-	_FONT_SIZE_PosLog,
-	_FONT_SIZE_PosPhys,
+	CALIBRATION_FONT_SIZE_Title,
+	CALIBRATION_FONT_SIZE_CircleName,
+	CALIBRATION_FONT_SIZE_PosLog,
+	CALIBRATION_FONT_SIZE_PosPhys,
 
-	_FONT_STYLE_Title,
-	_FONT_STYLE_CircleName,
-	_FONT_STYLE_PosLog,
-	_FONT_STYLE_PosPhys,
+	CALIBRATION_FONT_STYLE_Title,
+	CALIBRATION_FONT_STYLE_CircleName,
+	CALIBRATION_FONT_STYLE_PosLog,
+	CALIBRATION_FONT_STYLE_PosPhys,
 
-	_FONT_COLOR_Title,
-	_FONT_COLOR_CircleName,
-	_FONT_COLOR_PosLog,
-	_FONT_COLOR_PosPhys,
+	CALIBRATION_FONT_COLOR_Title,
+	CALIBRATION_FONT_COLOR_CircleName,
+	CALIBRATION_FONT_COLOR_PosLog,
+	CALIBRATION_FONT_COLOR_PosPhys,
 
-	_COLOR_BkScreen,
-	_COLOR_CircleFrame,
-	_COLOR_CircleFill,
-	_COLOR_CircleFillPress,
+	CALIBRATION_COLOR_BkScreen,
+	CALIBRATION_COLOR_CircleFrame,
+	CALIBRATION_COLOR_CircleFill,
+	CALIBRATION_COLOR_CircleFillPress,
 
-	_COEFF_COLOR_CircleName,
-	_COEFF_COLOR_PosLog,
-	_COEFF_COLOR_PosPhys,
+	CALIBRATION_COEFF_COLOR_CircleName,
+	CALIBRATION_COEFF_COLOR_PosLog,
+	CALIBRATION_COEFF_COLOR_PosPhys,
 
-	_DEBUG_ON
+	CALIBRATION_DEBUG_ON
 
 }_SCREEN_Calibration_enum;
 
@@ -44,7 +44,7 @@ void 	SCREEN_Calibration_main(void);
 
 int 	SCREEN_Calibration_funcGet(int offs);
 void 	SCREEN_Calibration_funcSet(int offs, int val);
-void 	SCREEN_Calibration_setDefaultParam(void);
-void 	SCREEN_Calibration_debug(void);
+void 	SCREEN_Calibration_setDefaultAllParam(void);
+void 	SCREEN_Calibration_printInfo(void);
 
 #endif /* LCD_SCREENS_SCREEN_CALIBRATIONLCD_H_ */
