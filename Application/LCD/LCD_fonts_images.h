7b4aa919643ccadb1720dbbc4a7670fc2182071d
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-07-10 15:43:48 +0200

    Update LCD_fonts_images.h

--- Application/LCD/LCD_fonts_images.h
+++ Application/LCD/LCD_fonts_images.h
@@ -327,6 +327,8 @@ void LCD_SetSpacesBetweenFonts(void);
 
 uint16_t LCD_Ypos(StructTxtPxlLen structTemp, int cmd, int offs);
 uint16_t LCD_Xpos(StructTxtPxlLen structTemp, int cmd, int offs);
+uint16_t LCD_posY(int nr, StructTxtPxlLen structTemp, int cmd, int offs);
+uint16_t LCD_posX(int nr, StructTxtPxlLen structTemp, int cmd, int offs);
 uint16_t LCD_Ymiddle(int nr, int cmd, uint32_t val);
 uint16_t LCD_Xmiddle(int nr, int cmd, uint32_t val, char *txt, int space, int constWidth);
 uint32_t SetPosAndWidth(uint16_t pos, uint16_t width);
