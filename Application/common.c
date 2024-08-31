954a335cb8f23264ba48179a20205c1b2d2d7519
marafal <marafal@interia.pl>
2024-08-31 22:14:38 +0200

    dfgfdg

---
+++ Application/common.c
@@ -0,0 +1,53 @@
+/*
+ * common.c
+ *
+ *  Created on: Aug 31, 2024
+ *      Author: maraf
+ */
+
+#include "stm32f7xx_hal.h"
+#include "common.h"
+
+int _ReturnVal(int val, int in){ return val; };
+int _ReturnVal2(int val, int in1,int in2){ return val; };
+int _ReturnVal3(int val, int in1,int in2,int in3){ return val; };
+
+void Int16ToCharBuff(char* buff, uint16_t val){
+	buff[0] = val>>8;
+	buff[1] = val;
+}
+void Int32ToCharBuff(char* buff, uint32_t val){
+	buff[0] = val>>24;
+	buff[1] = val>>16;
+	buff[2] = val>>8;
+	buff[3] = val;
+}
+uint16_t CharBuffToInt16(char* buff){
+	return ((buff[0]<<8) | buff[1]);
+}
+uint32_t CharBuffToInt32(char* buff){
+	return ((buff[0]<<24) | (buff[1]<<16) | (buff[2]<<8) | buff[3]);
+}
+
+int FV(VARIABLE_ACTIONS type, int nrMem, int val){
+	static int mem[10];
+	switch((int)type){
+		case SetVal:
+			mem[nrMem]=val;
+			return mem[nrMem];
+		case GetVal:
+			return mem[nrMem];
+		default:
+			return 0;
+}}
+int FV2(char* descr, VARIABLE_ACTIONS type, int nrMem, int val){
+	static int mem[10];
+	switch((int)type){
+		case SetVal:
+			mem[nrMem]=val;
+			return mem[nrMem];
+		case GetVal:
+			return mem[nrMem];
+		default:
+			return 0;
+}}
